a1fd0ee558e916db34eb19b5ef63903b59b0e9ce
Devansh Patel <149897528+DPdevanshpatel@users.noreply.github.com>
2026-01-30 15:15:50 +0530

    Time: 19 ms (26.68%), Space: 94.5 MB (34.71%) - LeetHub

--- 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
+++ 2022-convert-1d-array-into-2d-array/2022-convert-1d-array-into-2d-array.cpp
@@ -1,25 +1,71 @@
+// class Solution {
+// public:
+//     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+
+
+//         vector<vector<int>> mat(m,vector<int>(n));
+
+//         if(m*n==original.size()){
+//             // int row=original.size()/m;
+
+//             int index=0;
+//             for(int i=0; i<m; i++)
+//             {
+//                 for(int j=0; j<n; j++)
+//                 {
+//                     mat[i][j]=original[index];
+//                     index++;
+//                 }
+//             }
+//         }
+//         else{
+//             return {};
+//         }
+//         return mat;
+//     }
+// };
+
+// class Solution {
+// public:
+//     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+
+
+//         vector<vector<int>> mat(m,vector<int>(n));
+
+//         if(m*n==original.size()){
+//             // int row=original.size()/m;
+
+//             int index=0;
+//             int l=0;
+//             for(int i=0; i<m; i++)
+//             {
+//                 l=0;
+//                 while(l!=n){
+//                     mat[i][l]=original[index];
+//                     index++;
+//                     l++;
+//                 }
+//             }
+//         }
+//         else{
+//             return {};
+//         }
+//         return mat;
+//     }
+// };
+
 class Solution {
 public:
-    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
-
-
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n){
+       
         vector<vector<int>> mat(m,vector<int>(n));
 
-        if(m*n==original.size()){
-            // int row=original.size()/m;
-
-            int index=0;
-            for(int i=0; i<m; i++)
-            {
-                for(int j=0; j<n; j++)
-                {
-                    mat[i][j]=original[index];
-                    index++;
-                }
-            }
-        }
-        else{
-            return {};
+        if(m*n!=original.size()) return {};
+        for(int index=0; index<original.size(); index++)
+        {
+            int row = index/n;
+            int column = index%n;
+            mat[row][column] = original[index];
         }
         return mat;
     }
