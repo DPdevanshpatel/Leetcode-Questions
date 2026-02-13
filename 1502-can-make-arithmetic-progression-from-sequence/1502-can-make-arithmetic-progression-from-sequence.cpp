7dfbc2900c0bb4d7ea1701ed45604b1ebfde54de
Devansh Patel <149897528+DPdevanshpatel@users.noreply.github.com>
2026-02-13 13:25:02 +0530

    Time: 0 ms (100%), Space: 12.9 MB (13.65%) - LeetHub

---
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        
+        sort(arr.begin(),arr.end());
+        int m = arr[1]-arr[0];
+        for(int i=0; i<arr.size()-1; i++)
+        {
+            if(abs(arr[i]-arr[i+1])!=abs(m)){
+                return false;
+            }
+        }
+        return true;
+    }
+};
