c2bcc37150a37f40bbbbdd163fe6da78885ab9bf
Devansh Patel <149897528+DPdevanshpatel@users.noreply.github.com>
2026-08-04 13:58:15 +0530

    Time: 0 ms (100%), Space: 46.3 MB (76.12%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.java
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.java
@@ -1,36 +1,60 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+// /**
+//  * Definition for singly-linked list.
+//  * public class ListNode {
+//  *     int val;
+//  *     ListNode next;
+//  *     ListNode() {}
+//  *     ListNode(int val) { this.val = val; }
+//  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//  * }
+//  */
+// class Solution {
+//     public ListNode oddEvenList(ListNode head) {
+//         ListNode oddidx = new ListNode(-1);
+//         ListNode evenidx = new ListNode(-1);
+//         ListNode odddummy = oddidx;
+//         ListNode evendummy = evenidx;
+
+//         ListNode temp=head;
+//         int count=0;
+//         while(temp!=null){
+//             if(count%2==0){
+//                 ListNode a = new ListNode(temp.val);
+//                 odddummy.next=a;
+//                 odddummy=a;
+//             }else{
+//                 ListNode a = new ListNode(temp.val);
+//                 evendummy.next=a;
+//                 evendummy=a;
+//             }
+//             count++;
+//             temp=temp.next;
+//         }
+//         odddummy.next=evenidx.next;
+//         return oddidx.next;
+//     }
+// }
+
 class Solution {
     public ListNode oddEvenList(ListNode head) {
-        ListNode oddidx = new ListNode(-1);
-        ListNode evenidx = new ListNode(-1);
-        ListNode odddummy = oddidx;
-        ListNode evendummy = evenidx;
+        if(head==null) return null;
+        ListNode evenidx = head;
+        ListNode oddidx = head.next;
 
-        ListNode temp=head;
-        int count=0;
-        while(temp!=null){
-            if(count%2==0){
-                ListNode a = new ListNode(temp.val);
-                odddummy.next=a;
-                odddummy=a;
-            }else{
-                ListNode a = new ListNode(temp.val);
-                evendummy.next=a;
-                evendummy=a;
-            }
-            count++;
-            temp=temp.next;
+        if(oddidx==null) return head;
+        
+        ListNode tempeven = evenidx;
+        ListNode tempodd = oddidx;
+
+        while(tempodd!=null && tempodd.next!=null){
+            tempeven.next=tempodd.next;
+            tempeven=tempodd.next;
+            tempodd.next=tempeven.next;
+            tempodd=tempeven.next;
+
         }
-        odddummy.next=evenidx.next;
-        return oddidx.next;
+        tempeven.next=oddidx;
+
+        return evenidx;
     }
 }
