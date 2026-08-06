a2100542b261bbf22daf9e0ff43aad252c821393
Devansh Patel <149897528+DPdevanshpatel@users.noreply.github.com>
2026-08-06 22:50:56 +0530

    Time: 5 ms (43.06%), Space: 94.4 MB (68.08%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,22 +9,38 @@
  * }
  */
 class Solution {
-    public boolean isPalindrome(ListNode head) {
-        Stack<ListNode> st = new Stack<>();
-
+   public ListNode reverse(ListNode head) {
+        if(head==null || head.next==null) return head;
         ListNode temp = head;
-
-        while(temp!=null){
-            st.push(temp);
-            temp=temp.next;
+        ListNode curr= temp.next;
+        ListNode prev=null;
+        while(curr!=null){
+            curr= temp.next;
+            temp.next=prev;
+            prev=temp;
+            temp=curr;
         }
+        return prev;
+    }
 
-        while(head!=null){
-            if(st.peek().val!=head.val){
+    public boolean isPalindrome(ListNode head) {
+        
+        if(head.next==null) return true;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast.next!=null && fast.next.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+        ListNode second = reverse(slow.next);
+        ListNode start = head;
+        while(second!=null){
+            if(start.val!=second.val){
                 return false;
             }
-            head=head.next;
-            st.pop();
+            start=start.next;
+            second=second.next;
         }
         return true;
     }
