5026c9a5f77a624875318ffba9532c30de0724c5
Devansh Patel <149897528+DPdevanshpatel@users.noreply.github.com>
2026-08-29 01:57:56 +0530

    Time: 14 ms (32.7%), Space: 47.4 MB (87.46%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        
+        HashSet<Integer> set = new HashSet<>();
+        int[] arr = new int[2];
+        for(int num : nums){
+            if(set.contains(num)){
+                arr[0]=num;
+            }
+            set.add(num);
+        }
+
+        int sum=0;
+        for(int num:set){
+            sum+=num;
+        }
+
+        int sum2=0;
+        for(int i=1; i<=nums.length; i++){
+            sum2+=i;
+        }
+
+        arr[1]=sum2-sum;
+
+        return arr;
+    }
+}
